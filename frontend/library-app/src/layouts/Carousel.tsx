c0f8cf8af7b5ac3130c0c88ddd0f41c085ffb8dc
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-01 18:06:56 -0300

    Implemented mobile version for Carousel.tsx component

--- frontend/library-app/src/layouts/Carousel.tsx
+++ frontend/library-app/src/layouts/Carousel.tsx
@@ -57,6 +57,7 @@ export const Carousel = () => {
                                 </div>
                             </div>
                         </div>
+                        </div>
                         <button className="carousel-control-prev" type="button" 
                         data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
                             <span className="carousel-control-prev-icon" area-hidden='true'></span>
@@ -67,14 +68,36 @@ export const Carousel = () => {
                             <span className="carousel-control-next-icon" area-hidden='true'></span>
                             <span className="visually-hidden">Next</span>
                         </button>
-
-
-                        {/* MOBILE */}
-                        <div className="d-lg-none mt-3">
-                        
                         </div>
+
+
+ {/* MOBILE */}
+            <div className="d-lg-none mt-3">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-12 text-center">
+                        <img
+                            src={require('../Images/BooksImages/book-d-senvolvendo-sistemas.png')}
+                            width='151'
+                            height='233'
+                            alt="book"
+                        />
+                        <h6 className="mt-2">
+                            <b>Book</b>
+                        </h6>
+                        <p>D.SenvolvendoSistemas</p>
+                        <a className="btn main-color text-white" href="#">
+                            Reserve
+                        </a>
                     </div>
+                </div>
+                <div className="row mt-3 justify-content-center">
+                    <div className="col-12 text-center">
+                        <a className="btn btn-outline-secondary btn-lg" href="#">
+                            View More
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
