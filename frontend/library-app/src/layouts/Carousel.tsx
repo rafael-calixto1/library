cb50075f85c008bf1757d628689faffd331d9445
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-01 18:34:11 -0300

    Created ReturnBook component, also Imported ReturnBook component and integrated it into Carousel

--- frontend/library-app/src/layouts/Carousel.tsx
+++ frontend/library-app/src/layouts/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -11,64 +13,37 @@ export const Carousel = () => {
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <div className="row d-flex justify-content-center align-items-center ">
-                                <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-">
-                                     <div className="text-center ">
-                                     <img src={require('../Images/BooksImages/book-d-senvolvendo-sistemas.png')} 
-                                            width='151'
-                                            height='233'
-                                            alt="book"
-                                         />
-                                         <h6 className="mt-2">Book</h6>
-                                         <p>D.SenvolvendoSistemas</p>
-                                         <a className="btn main-color text-white" href="#">Reserve</a>
-                                     </div>
-                                </div>
+                                <ReturnBook/>
+                                <ReturnBook/>
+                                <ReturnBook/>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <div className="row d-flex justify-content-center align-items-center ">
-                                <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-">
-                                     <div className="text-center ">
-                                     <img src={require('../Images/BooksImages/book-d-senvolvendo-sistemas.png')} 
-                                            width='151'
-                                            height='233'
-                                            alt="book"
-                                         />
-                                         <h6 className="mt-2">Book</h6>
-                                         <p>D.SenvolvendoSistemas</p>
-                                         <a className="btn main-color text-white" href="#">Reserve</a>
-                                     </div>
-                                </div>
-                            </div>
+                                <ReturnBook/>
+                                <ReturnBook/>
+                                <ReturnBook/>  
+                            </div>                     
                         </div>
                         <div className="carousel-item ">
                             <div className="row d-flex justify-content-center align-items-center ">
-                                <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-">
-                                     <div className="text-center ">
-                                         <img src={require('../Images/BooksImages/book-d-senvolvendo-sistemas.png')} 
-                                            width='151'
-                                            height='233'
-                                            alt="book"
-                                         />
-                                         <h6 className="mt-2">Book</h6>
-                                         <p>D.SenvolvendoSistemas</p>
-                                         <a className="btn main-color text-white" href="#">Reserve</a>
-                                     </div>
-                                </div>
-                            </div>
+                                <ReturnBook/>
+                                <ReturnBook/>
+                                <ReturnBook/>  
+                            </div>  
                         </div>
-                        </div>
-                        <button className="carousel-control-prev" type="button" 
+                    </div>
+                    <button className="carousel-control-prev" type="button" 
                         data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
-                            <span className="carousel-control-prev-icon" area-hidden='true'></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" 
+                    <span className="carousel-control-prev-icon" area-hidden='true'></span>
+                    <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" 
                         data-bs-target='#carouselExampleControls' data-bs-slide='next'>
-                            <span className="carousel-control-next-icon" area-hidden='true'></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-                        </div>
+                    <span className="carousel-control-next-icon" area-hidden='true'></span>
+                    <span className="visually-hidden">Next</span>
+                    </button>
+            </div>
 
 
  {/* MOBILE */}
---
+++ frontend/library-app/src/layouts/ReturnBook.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const ReturnBook = () =>{
+    return (
+        <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-">
+            <div className="text-center ">
+                <img src={require('../Images/BooksImages/book-d-senvolvendo-sistemas.png')} 
+                    width='151'
+                    height='233'
+                    alt="book"
+                />
+                <h6 className="mt-2">Book</h6>
+                <p>D.SenvolvendoSistemas</p>
+                <a className="btn main-color text-white" href="#">Reserve</a>
+            </div>
+        </div>
+    );
+}
