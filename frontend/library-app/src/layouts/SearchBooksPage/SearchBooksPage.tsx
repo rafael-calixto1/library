03032a99d624bb581096e5c0ae95aadc10e7011b
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-07 18:05:40 -0300

    Implemented findByCategory functionality in SearchPageBooks. Added categorySelection state and categoryField function for value validation against database categories.

--- frontend/library-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ frontend/library-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -16,6 +16,7 @@ export const SearchBooksPage = () => {
     const [totalPages, setTotalPages] = useState(0);
     const [search, setSearch] = useState('');
     const [searchUrl, setSearchUrl] = useState('');
+    const [categorySelection, setCategorySelection] = useState('Book category');
 
     useEffect(() => {
         const fetchBooks  = async () => {
@@ -89,6 +90,21 @@ export const SearchBooksPage = () => {
         }
     };
 
+    const categoryField = (value: string) => {
+        if(
+            value.toLowerCase() === 'fe' ||
+            value.toLowerCase() === 'be' ||
+            value.toLowerCase() === 'data' ||
+            value.toLowerCase() === 'devops'
+        ){
+            setCategorySelection(value);
+            setSearchUrl(`/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`)
+        } else {
+            setCategorySelection('All')
+            setSearchUrl(`?page=0&size=${booksPerPage}`)
+        }
+    }
+
     const indexOfLastBook: number = currentPage * booksPerPage;
     const indexOfFirstBook : number = indexOfLastBook - booksPerPage;
     let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
@@ -118,30 +134,30 @@ export const SearchBooksPage = () => {
                                     id="dropdownMenuButton1" data-bs-toggle="dropdown"
                                     aria-expanded='false'
                                 >
-                                    Category
+                                    {categorySelection}
                                 </button>
                                 <ul className="dropdown-menu" aria-labelledby='dropdownMenuButton1'>
-                                    <li>
+                                    <li onClick={() => categoryField('All')}>
                                         <a href="#" className="dropdown-item">
                                             All
                                         </a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('FE')}>
                                         <a href="#" className="dropdown-item">
                                             Front End
                                         </a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('BE')}>
                                         <a href="#" className="dropdown-item">
                                             Backend
                                         </a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('Data')}>
                                         <a href="#" className="dropdown-item">
                                             Data
                                         </a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('Devops')}>
                                         <a href="#" className="dropdown-item">
                                             DevOps 
                                         </a>
