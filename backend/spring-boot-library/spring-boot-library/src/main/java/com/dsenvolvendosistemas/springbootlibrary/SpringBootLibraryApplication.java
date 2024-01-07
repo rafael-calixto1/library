63a2f4b92e81fd351cb4d3ae075daa3ed71792cd
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-07 12:38:26 -0300

    Modified BookRepository.java: Added findByTitleContaining method

--- backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/SpringBootLibraryApplication.java
+++ backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/SpringBootLibraryApplication.java
@@ -8,6 +8,7 @@ public class SpringBootLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLibraryApplication.class, args);
+
 	}
 
 }
--- backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/dao/BookRepository.java
+++ backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/dao/BookRepository.java
@@ -1,8 +1,11 @@
 package com.dsenvolvendosistemas.springbootlibrary.dao;
 
 import com.dsenvolvendosistemas.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
