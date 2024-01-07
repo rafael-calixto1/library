cbc168a8955953471c6e8b2a6768ea109f70d76a
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-07 12:13:49 -0300

    Refactored SearchBookPage to dynamically handle values

--- frontend/library-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ frontend/library-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -56,7 +56,8 @@ export const SearchBooksPage = () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    }, []);
+        window.scrollTo(0, 0)
+    }, [currentPage]);
 
 
     if(isLoading){
@@ -134,10 +135,10 @@ export const SearchBooksPage = () => {
                         </div>
                     </div>
                     <div className="mt-3">
-                        <h5>Number of results: (22)</h5>
+                        <h5>Number of results: ({totalAmountOfBooks})</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items: 
+                        {indexOfFirstBook + 1} to {indexOfLastBook} of {totalAmountOfBooks} items: 
                     </p>
                     {books.map(book => (
                         <SearchBook  book={book} key={book.id}/>
