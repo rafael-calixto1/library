745ebf615523d64a29b363d34e570fb719c83cad
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-07 18:56:15 -0300

    Added ReactRouter to the project and implemented it

--- frontend/library-app/src/App.tsx
+++ frontend/library-app/src/App.tsx
@@ -4,13 +4,28 @@ import { Navbar } from './layouts/NavBarAndFooter/Navbar';
 import { Footer } from './layouts/NavBarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/components/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <>
-      <Navbar/>
-      {/*<HomePage/>*/}
-      <SearchBooksPage/>
+      <Navbar/>  
+      <Switch>
+  
+        <Route path='/' exact>
+          <Redirect to = "/home"/>
+        </Route>
+        
+        <Route>
+          <HomePage></HomePage>
+        </Route>
+ 
+        <Route path='/search'>
+          <SearchBooksPage/>
+        </Route>
+     
+      </Switch>
+     
       <Footer/> 
     </>
     );
