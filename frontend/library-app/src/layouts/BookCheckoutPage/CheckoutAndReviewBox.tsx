5f4b750a92d203fee2578e267a3fd5afb25716e6
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-02-04 16:04:56 -0300

    Fix typos in BookCheckoutPage component

--- frontend/library-app/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ frontend/library-app/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -7,12 +7,12 @@ export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobil
             <div className="card-boy container">
                 <div className="mt-3">
                     <p>
-                        <b>0/5</b>
+                        <b>0/5 </b>
                         books checked out 
                     </p>
                     <hr />
                     {props.book && props.book.copiesAvailable && props.book.copiesAvailable > 0 ?
-                        <h4 className="text-sucess">
+                        <h4 className="text-success">
                             Available
                         </h4>
                         :
@@ -22,10 +22,12 @@ export const CheckoutAndReviewBox: React.FC<{ book: BookModel | undefined, mobil
                     }
                     <div className="row">
                         <p className="col-6 lead">
-                            <b>{props.book?.copies}</b>
+                            <b>{props.book?.copies} </b>
+                            copies
                         </p>
                         <p className="col-6 lead">
-                            <b>{props.book?.copiesAvailable}</b>
+                            <b>{props.book?.copiesAvailable} </b>
+                            available
                         </p>
                     </div>
                 </div>
