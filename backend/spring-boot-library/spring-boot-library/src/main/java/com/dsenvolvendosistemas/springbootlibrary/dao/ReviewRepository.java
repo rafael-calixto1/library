2b76260b6856e3f844eaa85e39bbfbd05f88b62f
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-02-04 16:38:00 -0300

    add find Review by Id of a book

--- backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/dao/ReviewRepository.java
+++ backend/spring-boot-library/spring-boot-library/src/main/java/com/dsenvolvendosistemas/springbootlibrary/dao/ReviewRepository.java
@@ -1,9 +1,14 @@
 package com.dsenvolvendosistemas.springbootlibrary.dao;
 
 import com.dsenvolvendosistemas.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
 
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId,
+                              Pageable pageable);
 }
 
